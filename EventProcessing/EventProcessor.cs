6a21243377671e4ade2d66c58107be288b722123
Marcin <marcin.jarek.frackiewicz@gmail.com>
2021-10-31 19:36:59 +0000

    Event Processor added

---
+++ EventProcessing/EventProcessor.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Text.Json;
+using AutoMapper;
+using CommandService.Data;
+using CommandService.Dtos;
+using CommandService.Models;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CommandService.EventProcessing
+{
+    public class EventProcessor : IEventProcessor
+    {
+        private readonly IServiceScopeFactory _scopeFactory;
+        private readonly IMapper _mapper;
+
+        public EventProcessor(IServiceScopeFactory scopeFactory, IMapper mapper)
+        {
+            _scopeFactory = scopeFactory;
+            _mapper = mapper;
+        }
+
+        public void ProcessEvent(string message)
+        {
+            var eventType = DetermineEvent(message);
+
+            switch (eventType)
+            {
+                case EventType.PlatformPublished:
+
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void addPlatform(string platformPublishedMessage)
+        {
+            using (var scope = _scopeFactory.CreateScope())
+            {
+                var repository = scope.ServiceProvider.GetRequiredService<ICommandRepo>();
+
+                var platformPublishedDto = JsonSerializer.Deserialize<PlatformPublishedDto>(platformPublishedMessage);
+
+                try
+                {
+                    var platform = _mapper.Map<Platform>(platformPublishedDto);
+                    if (!repository.ExternalPlatformExists(platform.ExternalId))
+                    {
+                        repository.CreatePlatform(platform);
+                        repository.SaveChanges();
+                    }
+                    else
+                    {
+                        Console.WriteLine("--> Platform already exists");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"--> Could not add Platform to DB {ex.Message}");
+                }
+            }
+        }
+
+        private EventType DetermineEvent(string notificationMessage)
+        {
+            Console.WriteLine("--> Determining Event");
+
+            var eventType = JsonSerializer.Deserialize<GenericEventDto>(notificationMessage);
+
+            switch(eventType.Event)
+            {
+                case "Platform_Published":
+                    Console.WriteLine("Platform Published Event Detected");
+                    return EventType.PlatformPublished;
+                default:
+                    Console.WriteLine("--> Could not determine the event type");
+                    return EventType.Undetermined;
+            }
+        }
+    }
+
+    enum EventType
+    {
+        PlatformPublished,
+        Undetermined
+    }
+}
